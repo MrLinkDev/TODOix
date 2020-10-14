be63db42cf8279c6887f1e3abd3918aa1aa4cee6
Alexander <sasha2000.gorbunov@gmail.com>
2020-10-14 22:01:10 +0500

    Были добавлены дополнительные материалы

---
+++ TODOix App/src/main/java/ru/link/todoix/TodOixApplication.java
@@ -0,0 +1,12 @@
+package ru.link.todoix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodOixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodOixApplication.class, args);
+    }
+}
---
+++ TODOix App/src/test/java/ru/link/todoix/TodOixApplicationTests.java
@@ -0,0 +1,12 @@
+package ru.link.todoix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodOixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
